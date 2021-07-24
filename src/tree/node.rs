76629fa2e6094a9691864ed71fc7524ffd450a93
Gabriel Sanches <gabriel@gsr.dev>
2021-07-24 19:07:40 -0300

    tree/node: Return error when adding path

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -1,5 +1,18 @@
 use std::{ffi::OsStr, path::PathBuf};
 
+use thiserror::Error;
+
+/// Possible errors when adding nodes.
+#[derive(Debug, Error, PartialEq)]
+pub enum AddError {
+	#[error("node for {0:?} is leaf, not branch")]
+	LeafAsBranch(PathBuf),
+	#[error("leaf already exists for {0:?}")]
+	LeafExists(PathBuf),
+}
+
+pub type AddResult = Result<(), AddError>;
+
 #[derive(Debug, PartialEq)]
 pub enum Node {
 	Root(Vec<Node>),
@@ -9,7 +22,7 @@ pub enum Node {
 
 impl Node {
 	/// Adds a path to the node if and only if a node for that path doesn't exist yet.
-	pub fn add(&mut self, path: PathBuf) {
+	pub fn add(&mut self, path: PathBuf) -> AddResult {
 		// Let's break the path into segments.
 		let segments = path.iter().collect::<Vec<&OsStr>>();
 
@@ -23,12 +36,10 @@ impl Node {
 					// Let's check whether there's already a node with same name, otherwise let's
 					// just create it.
 					let child = children.iter_mut().find(|node| node.get_path() == segment);
-					let node_exists = child.is_some();
 
 					if rest.is_empty() {
-						if node_exists {
-							// TODO(gbrlsnchs): Handle this with an error maybe?
-							return;
+						if child.is_some() {
+							return Err(AddError::LeafExists(segment.into()));
 						}
 
 						children.push(Self::Leaf {
@@ -37,22 +48,22 @@ impl Node {
 					} else {
 						let rest = rest.iter().collect();
 						if let Some(branch) = child {
-							branch.add(rest);
+							branch.add(rest)?;
 						} else {
 							let mut branch = Node::Branch {
 								path: segment.into(),
 								children: Vec::new(),
 							};
-							branch.add(rest);
+							branch.add(rest)?;
 							children.push(branch);
 						}
 					}
 				}
-				_ => {
-					unimplemented!();
-				}
+				_ => return Err(AddError::LeafAsBranch(segment.into())),
 			};
 		}
+
+		Ok(())
 	}
 
 	/// Returns the segment path for the node. Root panics.
@@ -74,34 +85,65 @@ mod tests {
 	#[test]
 	fn test_add() {
 		struct Test {
-			node: Node,
+			node_before: Node,
 			input: PathBuf,
-			want: Node,
+			node_after: Node,
+			want: AddResult,
 		}
 
 		let test_cases = vec![
 			Test {
-				node: Node::Root(Vec::new()),
+				node_before: Node::Root(Vec::new()),
 				input: PathBuf::from("foo"),
-				want: Node::Root(vec![Node::Leaf {
+				node_after: Node::Root(vec![Node::Leaf {
 					path: PathBuf::from("foo"),
 				}]),
+				want: Ok(()),
 			},
 			Test {
-				node: Node::Root(Vec::new()),
+				node_before: Node::Root(Vec::new()),
 				input: PathBuf::from("foo/bar"),
-				want: Node::Root(vec![Node::Branch {
+				node_after: Node::Root(vec![Node::Branch {
 					path: PathBuf::from("foo"),
 					children: vec![Node::Leaf {
 						path: PathBuf::from("bar"),
 					}],
 				}]),
+				want: Ok(()),
+			},
+			Test {
+				node_before: Node::Root(vec![Node::Leaf {
+					path: PathBuf::from("foo"),
+				}]),
+				input: PathBuf::from("foo"),
+				node_after: Node::Root(vec![Node::Leaf {
+					path: PathBuf::from("foo"),
+				}]),
+				want: Err(AddError::LeafExists(PathBuf::from("foo"))),
+			},
+			Test {
+				node_before: Node::Root(vec![Node::Branch {
+					path: PathBuf::from("foo"),
+					children: vec![Node::Leaf {
+						path: PathBuf::from("bar"),
+					}],
+				}]),
+				input: PathBuf::from("foo"),
+				node_after: Node::Root(vec![Node::Branch {
+					path: PathBuf::from("foo"),
+					children: vec![Node::Leaf {
+						path: PathBuf::from("bar"),
+					}],
+				}]),
+				want: Err(AddError::LeafExists(PathBuf::from("foo"))),
 			},
 		];
 
 		for mut case in test_cases.into_iter() {
-			case.node.add(case.input);
-			assert_eq!(case.want, case.node);
+			let got = case.node_before.add(case.input);
+
+			assert_eq!(got, case.want);
+			assert_eq!(case.node_after, case.node_before);
 		}
 	}
 }
