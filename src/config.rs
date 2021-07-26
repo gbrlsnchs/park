9b5845469a3240fdb2032b1ea8bc4083eea889db
Gabriel Sanches <gabriel@gsr.dev>
2021-07-25 22:39:37 -0300

    config: Add base directory as option

--- src/config.rs
+++ src/config.rs
@@ -11,6 +11,32 @@ pub struct Config {
 /// Represents all possible modifications that can be made to links.
 #[derive(Default)]
 pub struct Options {
+	/// The base directory of a target's link.
+	pub base_dir: BaseDir,
 	/// A different name for a target's link.
 	pub link_name: OsString,
+}
+
+/// Represents the base directory for a link. Its meaning is up to the application.
+#[derive(Debug, PartialEq)]
+pub enum BaseDir {
+	Config,
+	Cache,
+	Data,
+	Home,
+	Bin,
+	Documents,
+	Download,
+	Desktop,
+	Pictures,
+	Music,
+	Videos,
+	Templates,
+	PublicShare,
+}
+
+impl Default for BaseDir {
+	fn default() -> Self {
+		Self::Config
+	}
 }
