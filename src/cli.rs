6e53f86abcbabfce75b8e4dce5a00e5cc05d4123
Gabriel Sanches <gabriel@gsr.dev>
2022-01-17 11:52:44 -0300

    cli: Enable linking functionality
    Also, runtime tags don't need a plus sign anymore, they're simply
    regular arguments.

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::error::Error;
 use std::ffi::OsString;
 use std::io::{self, Read, Write};
@@ -12,7 +11,14 @@ use crate::tree::Tree;
 pub type Result = StdResult<(), Box<dyn Error>>;
 
 #[derive(Parser)]
-pub struct Args {}
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+	#[clap(long, short, help = "Try to link eligible targets")]
+	link: bool,
+
+	#[clap(parse(from_os_str), help = "List of additional tags")]
+	tags: Vec<OsString>,
+}
 
 /// Runs the program, parsing STDIN for a config file.
 pub fn run() -> Result {
@@ -24,18 +30,13 @@ pub fn run() -> Result {
 
 	let config: Config = toml::from_str(&input)?;
 
-	let (runtime_tags, flags): (Vec<OsString>, Vec<OsString>) = env::args_os()
-		.skip(1)
-		.partition(|arg| arg.to_string_lossy().starts_with('+'));
-
-	let _args = Args::parse_from(flags);
+	let args = Args::parse();
 
 	let tree = Tree::parse(
 		config,
-		runtime_tags
+		args.tags
 			.iter()
-			.map(|tag| tag.to_string_lossy().trim_start_matches('+').into())
-			.filter(|tag: &String| !tag.is_empty())
+			.map(|tag| tag.to_string_lossy().into())
 			.collect(),
 	)?;
 
@@ -44,7 +45,11 @@ pub fn run() -> Result {
 	let stdout = io::stdout();
 	let mut handle = stdout.lock();
 
-	write!(handle, "{}", tree)?;
+	if args.link {
+		let _results = tree.link()?;
+	} else {
+		write!(handle, "{}", tree)?;
+	}
 
 	Ok(())
 }
