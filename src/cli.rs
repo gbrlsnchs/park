578851e7fe83cec5de3599da48e9532328c3faad
Gabriel Sanches <gabriel@gsr.dev>
2022-01-20 16:34:42 -0300

    cli: Add linking test

--- src/cli.rs
+++ src/cli.rs
@@ -45,7 +45,7 @@ pub fn run(input: &str, mut stdout: impl Write, args: Args) -> Result {
 
 #[cfg(test)]
 mod tests {
-	use std::env;
+	use std::{env, fs, path::PathBuf};
 
 	use ansi_term::Colour;
 	use indoc::indoc;
@@ -59,6 +59,8 @@ mod tests {
 			base_dir = "tests"
 
 			[targets.foo]
+			tags.all_of = ["foo"]
+
 			[targets.bar]
 		"#};
 		let mut stdout = Vec::new();
@@ -74,15 +76,12 @@ mod tests {
 			format!(
 				indoc! {"
 				.       {equals} {current_dir}
-				{t_bar}bar {arrow} {bar} {ready}
-				{l_bar}foo {arrow} {foo} {ready}
+				{l_bar}bar {arrow} {bar} {ready}
 			"},
-				t_bar = symbols_color.paint("├── "),
 				l_bar = symbols_color.paint("└── "),
 				equals = symbols_color.paint(":="),
 				arrow = symbols_color.paint("<-"),
 				current_dir = Colour::Cyan.paint(current_dir.to_string_lossy()),
-				foo = link_color.paint("tests/foo"),
 				bar = link_color.paint("tests/bar"),
 				ready = Colour::Green.bold().paint("(READY)"),
 			)
@@ -134,6 +133,37 @@ mod tests {
 				ready = Colour::Green.bold().paint("(READY)"),
 			)
 		);
+
+		Ok(())
+	}
+
+	#[test]
+	fn test_linking() -> Result {
+		let input = indoc! {r#"
+			base_dir = "tests"
+
+			[targets.foo]
+			tags.all_of = ["foo"]
+
+			[targets.bar]
+		"#};
+		let mut stdout = Vec::new();
+
+		run(
+			input,
+			&mut stdout,
+			Args {
+				link: true,
+				..Args::default()
+			},
+		)?;
+
+		let link_path = "tests/bar";
+		let link = PathBuf::from(link_path).read_link();
+		fs::remove_file(link_path)?;
+
+		assert!(link.is_ok());
+		assert_eq!(String::from_utf8(stdout).unwrap(), "");
 
 		Ok(())
 	}
