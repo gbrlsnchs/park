24c06461ab44f4ee413359a1f8c4eed19bf44436
Gabriel Sanches <gabriel@gsr.dev>
2022-12-18 17:32:45 -0300

    Make manpages more consistent

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::{
 	fs,
 	io::{Error, Write},
-	path::PathBuf,
+	path::{Path, PathBuf},
 	process::{Command, Stdio},
 };
 
@@ -29,8 +29,27 @@ fn main() -> Result<(), Error> {
 		clap_complete::generate_to(*shell, &mut app, &app_name, &completions_dir)?;
 	}
 
+	if Command::new("scdoc").spawn().is_err() {
+		eprintln!("scdoc not found in PATH, skipping generating manpage templates from doc/");
+
+		return Ok(());
+	}
+
+	build_manpages(app, &target_dir)?;
+
+	Ok(())
+}
+
+fn build_manpages(app: App, target_dir: &Path) -> Result<(), Error> {
 	let doc_dir = target_dir.join("doc");
 	fs::create_dir_all(&doc_dir)?;
+
+	let man = Man::new(app);
+	let mut buffer: Vec<u8> = Default::default();
+	man.render_name_section(&mut buffer)?;
+	man.render_synopsis_section(&mut buffer)?;
+	man.render_description_section(&mut buffer)?;
+	man.render_options_section(&mut buffer)?;
 
 	for doc in fs::read_dir("doc")? {
 		let doc = doc?;
@@ -39,11 +58,6 @@ fn main() -> Result<(), Error> {
 			.stdin(Stdio::piped())
 			.stdout(Stdio::piped())
 			.spawn();
-
-		if cmd.is_err() {
-			eprintln!("scdoc not found in PATH, skipping generating manpage templates from doc/");
-			return Ok(());
-		}
 
 		let mut cmd = cmd?;
 
@@ -56,31 +70,13 @@ fn main() -> Result<(), Error> {
 		let doc = PathBuf::from(doc.file_name());
 		let doc = doc.file_stem().unwrap();
 
-		fs::write(doc_dir.join(doc), output.stdout)?;
+		let content = std::str::from_utf8(&output.stdout).unwrap();
+		let content = content
+			.replace("{{data}}", std::str::from_utf8(&buffer).unwrap())
+			.replace("\n\n.P", "\n.P");
+
+		fs::write(doc_dir.join(doc), content)?;
 	}
-
-	let man = Man::new(app);
-	let mut buffer: Vec<u8> = Default::default();
-	man.render_title(&mut buffer)?;
-	man.render_name_section(&mut buffer)?;
-	man.render_synopsis_section(&mut buffer)?;
-	man.render_description_section(&mut buffer)?;
-	man.render_options_section(&mut buffer)?;
-
-	let footer: String = r".SH SEE ALSO
-.P
-\fIpark\fR(5)
-.P
-.SH AUTHORS
-.P
-Developed and maintained by Gabriel Sanches <gabriel@gsr.\&dev>.\&
-.P
-Source code is located at <https://git.\&sr.\&ht/~gbrlsnchs/park>.\&"
-		.into();
-	buffer.append(&mut footer.into_bytes());
-
-	let manpage = PathBuf::from(format!("{}.1", app_name));
-	fs::write(PathBuf::from(&doc_dir).join(manpage), buffer)?;
 
 	Ok(())
 }
---
+++ doc/park.1.sdc
@@ -0,0 +1,13 @@
+park(1)
+
+{{data}}
+
+# SEE ALSO
+
+_park_(5)
+
+# AUTHORS
+
+Developed and maintained by Gabriel Sanches <gabriel@gsr.dev>.
+
+Source code is located at <https://git.sr.ht/~gbrlsnchs/park>.
